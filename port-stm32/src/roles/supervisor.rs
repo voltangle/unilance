1f07cb71f0db78bdabcb0f20888efc1bbfc23627
GGorAA <ggoraa1029@gmail.com>
2026-02-15 16:06:04 +0100

    fix(port-stm32): forgor

--- port-stm32/src/roles/supervisor.rs
+++ port-stm32/src/roles/supervisor.rs
@@ -113,20 +113,3 @@ impl Storage for RamStorage {
     }
 }
 
-// TODO: make it not so retarded
-
-#[derive(Clone, Copy)]
-pub struct ControlConfig {
-    pub kp: u16,
-    pub kp_expo: f32,
-    pub ki: u16,
-    pub kd_fore: u16,
-    pub kd_aft: u16,
-    pub setpoint_zero: f32,
-    pub integral_max: f32,
-    pub integral_min: f32,
-
-    pub max_phase_current: u16,
-    pub min_phase_current: u16,
-
-}
