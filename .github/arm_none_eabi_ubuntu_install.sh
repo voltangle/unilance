0d07155b0e855d870df6b1329ed172287cc3c41e
GGorAA <ggoraa1029@gmail.com>
2026-02-20 18:54:19 +0100

    fix(ci): attempt no 6

--- .github/arm_none_eabi_ubuntu_install.sh
+++ .github/arm_none_eabi_ubuntu_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 curl -LO https://developer.arm.com/-/media/Files/downloads/gnu/15.2.rel1/binrel/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi.tar.xz
-sudo tar xjf arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi.tar.xz -C /usr/share/
+sudo tar -xvf arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi.tar.xz -C /usr/share/
 sudo ln -s /usr/share/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gcc /usr/bin/arm-none-eabi-gcc
 sudo ln -s /usr/share/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-g++ /usr/bin/arm-none-eabi-g++
 sudo ln -s /usr/share/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
