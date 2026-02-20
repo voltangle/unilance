3508225adb55aeed4d67b1088bafe5527b464e16
GGorAA <ggoraa1029@gmail.com>
2026-02-20 18:52:14 +0100

    fix(ci): attempt no 5

--- .github/arm_none_eabi_ubuntu_install.sh
+++ .github/arm_none_eabi_ubuntu_install.sh
@@ -7,3 +7,4 @@ sudo ln -s /usr/share/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-n
 sudo ln -s /usr/share/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-gdb /usr/bin/arm-none-eabi-gdb
 sudo ln -s /usr/share/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-size /usr/bin/arm-none-eabi-size
 sudo ln -s /usr/share/arm-gnu-toolchain-15.2.rel1-x86_64-arm-none-eabi/bin/arm-none-eabi-objcopy /usr/bin/arm-none-eabi-objcopy
+export PATH="${PATH}:/usr/bin/"
