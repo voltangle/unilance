330cba9cd5897bd3aa03bb235672a5bacd547cfa
GGorAA <ggoraa1029@gmail.com>
2026-03-12 16:34:47 +0100

    feature(input): add stubs for dials

--- core-supervisor/src/input/mod.rs
+++ core-supervisor/src/input/mod.rs
@@ -8,11 +8,18 @@ pub use power_aux::*;
 #[allow(unused)]
 pub trait InputMethods {
     fn is_pressed(role: ButtonRole) -> bool;
+    /// Returns how far did the dial (based on a relative encoder) move relative to when
+    /// this was called last time.
+    fn dial_relative_distance() -> i16;
+    /// Returns the dials' absolute position. Only for absolute encoders/potentiometers.
+    fn dial_absolute_position() -> i32;
 }
 
 #[allow(unused)]
 unsafe extern "Rust" {
-    pub(crate) fn unilance_input_is_pressed(role: ButtonRole) -> bool;
+    pub fn unilance_input_is_pressed(role: ButtonRole) -> bool;
+    pub fn unilance_input_dial_relative_distance() -> i16;
+    pub fn unilance_input_dial_absolute_position() -> i32;
 }
 
 #[allow(unused)]
